1a3a6e979bbfb85901fca433ee135d500ad79893
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-12-03 13:57:45 +0100

    accordion menu with links added to navbar for activity

---
+++ client/src/components/NavbarActivities.tsx
@@ -0,0 +1,77 @@
+// src/components/NavbarHome.tsx
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import React from "react";
+
+const NavbarActivities: React.FC = () => {
+	return (
+		<AppBar
+			position="static"
+			sx={{
+				backgroundColor: "white",
+				color: "black",
+				boxShadow: "none",
+				borderBottom: "1px solid #e0e0e0",
+			}}
+		>
+			<Toolbar>
+				<Box sx={{ flexGrow: 1, display: "flex", alignItems: "center" }}>
+					<Typography variant="h6" component="div">
+						ParentsConnect
+					</Typography>
+				</Box>
+				{/* Länkar */}
+				<Box sx={{ display: "flex", gap: 3 }}>
+					<Button sx={{ textTransform: "none" }}>Aktiviteter Hemma</Button>
+					<Button sx={{ textTransform: "none" }}>Föräldraforum</Button>
+					<Button sx={{ textTransform: "none" }}>Produkter & Rabatter</Button>
+
+					{/* Mer information med Accordion */}
+					<Accordion
+						sx={{
+							display: "inline-block",
+							boxShadow: "none",
+							width: "auto",
+							margin: 0,
+							backgroundColor: "transparent",
+						}}
+					>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls="panel1a-content"
+							id="panel1a-header"
+							sx={{ padding: 0 }}
+						>
+							<Button sx={{ textTransform: "none" }}>Mer information</Button>
+						</AccordionSummary>
+						<AccordionDetails sx={{ padding: "8px 16px" }}>
+							<Box>
+								<Button
+									sx={{
+										textTransform: "none",
+										display: "block",
+										marginBottom: 1,
+									}}
+								>
+									Kontakta oss
+								</Button>
+								<Button sx={{ textTransform: "none", display: "block" }}>
+									Användartips
+								</Button>
+							</Box>
+						</AccordionDetails>
+					</Accordion>
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+export default NavbarActivities;
