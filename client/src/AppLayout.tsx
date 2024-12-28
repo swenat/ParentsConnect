336e840fcec33382b0d4fd3363621f5ae03431d8
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-12-28 17:19:49 +0100

    removal of unnecessary files

--- client/src/AppLayout.tsx
+++ client/src/AppLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import "./App.css";
 import Activity from "./pages/Activity";
 import Home from "./pages/Home";
 import LoginUser from "./pages/LoginUser";
