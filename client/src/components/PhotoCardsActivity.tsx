634b363ff49c0407a352044ade1db877bdc3537f
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-12-28 17:25:45 +0100

    change of imagelinks

--- client/src/components/PhotoCardsActivity.tsx
+++ client/src/components/PhotoCardsActivity.tsx
@@ -21,8 +21,8 @@ const PhotoCardsActivity: React.FC = () => {
 			<Card sx={{ maxWidth: { xs: "100%", sm: 345 } }}>
 				<CardMedia
 					sx={{ height: 220 }}
-					image="https://am3pap004files.storage.live.com/y4m0W8CtGrtow8uvdvXmqezVrghGdd8PwBYU8pk8w1l16kJJAeg7ow-w5_Zw6mRPvnUNj9yA6tT3yFqBUC7WHCir5W7qzWq40UU1uD3_aVScZtaY_tzqhRiHRK8tL15wfWSiyVdlPle8QrIvUUfM_HSvR-458_rMZEr9sHgCh6S-VW7ZTvPYvlhYgwAWxL64JGnVicdd1Pnlk40lllOlebrdmX3sDZgfP_6EmSnJIjCMNg?encodeFailures=1&width=1024&height=1024"
-					title="föräldrar-i-parken"
+					image="https://cdn.pixabay.com/photo/2023/03/12/13/27/mountains-7846880_1280.jpg"
+					title="människor hajkande ett isberg"
 				/>
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="div">
@@ -39,8 +39,8 @@ const PhotoCardsActivity: React.FC = () => {
 			<Card sx={{ maxWidth: { xs: "100%", sm: 345 } }}>
 				<CardMedia
 					sx={{ height: 220 }}
-					image="https://am3pap004files.storage.live.com/y4mgRtUHznhleYNM93tOzW-Z6rwrAQi8F3-Pb3s0sZkRDPplW3c44hTBRP47YEPVJv_-w75hoBDEvsY75zQn95V3caogaIw5Rd2Xy0z3htsGSHGb_u2eZd6ebEd6_LY76J45bxEw8rtQq1BUDWnFZQmwad4tEyEZV6yyRFss7LQueeFNj6lY8yNxvgbTqujBlNBHEABEZxqRQk8Nv5k79dOMecmbi8lrHN6IYPuQy7XJH8?encodeFailures=1&width=1024&height=1024"
-					title="pappor-barnvagnspromenad"
+					image="https://cdn.pixabay.com/photo/2016/01/06/21/37/dad-1125002_1280.jpg"
+					title="pappa-barnpromenad"
 				/>
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="div">
@@ -57,8 +57,8 @@ const PhotoCardsActivity: React.FC = () => {
 			<Card sx={{ maxWidth: { xs: "100%", sm: 345 } }}>
 				<CardMedia
 					sx={{ height: 220 }}
-					image="https://am3pap004files.storage.live.com/y4mJ8_gbn_CsrJ9O5Y-XbwldMTsxNim6OLkHsEvor-sxjDX5c0FLqwDO0rLNDwmI7w60BqV1_RUFpVgnZ2PNlNpg_PDXPOMNLKAiJMvz6K4AmllJMkpgvp1_Pu1VFJsgtc__OUkIR4mnke5d-2gxaHDBjo7HObG22gC3AoEGm7VzXy15r4Vh-oGetW-E5QAmHEGphGXJGn48ZD5umjQOhnZJLa5AknKC4Y7EneEohWLerg?encodeFailures=1&width=1024&height=1024"
-					title="föräldrar-lekplats-med-barn"
+					image="https://cdn.pixabay.com/photo/2017/06/27/21/13/team-spirit-2448837_1280.jpg"
+					title="händer korsade med varandra"
 				/>
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="div">
