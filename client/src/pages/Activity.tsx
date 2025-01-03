e719251d5db0e5efb85db4caf4ac4f560304f010
Nathalie Lysefeldt <lysefeldt@outlook.com>
2025-01-03 20:40:49 +0100

    added padding

--- client/src/pages/Activity.tsx
+++ client/src/pages/Activity.tsx
@@ -179,7 +179,7 @@ const Activity: React.FC = () => {
 						borderRadius: 2,
 						maxWidth: "1200px",
 						margin: "0 auto",
-						paddingLeft: { xs: "9px", lg: "32px" },
+						paddingLeft: { xs: "9px", lg: "35px" },
 					}}
 				/>
 
@@ -190,7 +190,7 @@ const Activity: React.FC = () => {
 						alignItems: { xs: "center", md: "flex-start" },
 						textAlign: { xs: "center", md: "left" },
 						marginTop: "20px",
-						paddingLeft: { xs: "9px", lg: "32px" },
+						paddingLeft: { xs: "9px", lg: "35px" },
 					}}
 				>
 					<Typography
